28200e2d7fb161aefe85fcfc9c5affeeb3c01a23
niteshrai30 <niteshrai2440@gmail.com>
2023-01-05 16:12:59 +0530

    Code with Nitesh

---
+++ tut1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+
+     // std ::cout << "Hello World  \n" ;    // when namespace std is not declared.
+        cout << "Hello World  \n" ;       
+            
+        
+      //   if we donot declare std in namespace then we have to do scope resolution of std,std::cout,
+      //   we have to do either define std in namespace or define scope resolution of std,
+
+      return 0;    // successful termination of the function.
+}
